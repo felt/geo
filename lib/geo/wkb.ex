fe9b28b02b15fc2310ada88fa3abc11dda1eef68
Bryan Joseph <bryanjos@gmail.com>
2014-03-12 23:29:37 -0500

    Encoding and decoding of WKB and EWKB now work for points, lines, and polygons

--- lib/geo/wkb.ex
+++ lib/geo/wkb.ex
@@ -13,39 +13,55 @@ defmodule Geo.WKB do
       srid = integer_to_binary(geom.srid, 16) |> Geo.Utils.pad_left(8)
     end
 
-    x = geom.coordinates
-        |> hd
-        |> Geo.Utils.float_to_hex(64)
-        |> integer_to_binary(16)
-
-    y = geom.coordinates
-        |> List.last
-        |> Geo.Utils.float_to_hex(64)
-        |> integer_to_binary(16)
-
     if endian == :ndr do
       type = Geo.Utils.reverse_byte_order(type)
       srid = Geo.Utils.reverse_byte_order(srid)
+    end
+
+    "#{endian_hex}#{type}#{srid}" <> encode_coordinates(geom.type, endian, geom.coordinates)
+  end
+
+  defp encode_coordinates(:point, endian, coordinates) do
+
+    x = coordinates |> hd |> Geo.Utils.float_to_hex(64) |> integer_to_binary(16)
+    y = coordinates |> List.last |> Geo.Utils.float_to_hex(64) |> integer_to_binary(16)
+
+    if endian == :ndr do
       x = Geo.Utils.reverse_byte_order(x)
       y = Geo.Utils.reverse_byte_order(y)
     end
 
-    "#{endian_hex}#{type}#{srid}#{x}#{y}"
+    "#{x}#{y}"
   end
 
-  def type_to_hex(type, include_srid \\ false) do
-    value = if include_srid, do: 0x20000000, else: 0x00000000
+  defp encode_coordinates(:line_string, endian, coordinates) do
+    number_of_points = integer_to_binary(length(coordinates), 16)
+                       |> Geo.Utils.pad_left(8)
 
-    case type do
-      :point ->
-        value + 0x01
-      :line_string ->
-        value + 0x02
-      :polygon ->
-        value + 0x03
-      true ->
-        value + 0x04
+    if endian == :ndr do
+      number_of_points = Geo.Utils.reverse_byte_order(number_of_points)
     end
+
+    points = Enum.map(coordinates, fn(pair) ->
+      encode_coordinates(:point, endian, pair)
+    end) |> Enum.join
+
+    number_of_points <> points
+  end
+
+  defp encode_coordinates(:polygon, endian, coordinates) do
+    number_of_lines = integer_to_binary(length(coordinates), 16)
+                       |> Geo.Utils.pad_left(8)
+
+    if endian == :ndr do
+      number_of_lines = Geo.Utils.reverse_byte_order(number_of_lines)
+    end
+
+    points = Enum.map(coordinates, fn(line) ->
+      encode_coordinates(:line_string, endian, line)
+    end) |> Enum.join
+
+    number_of_lines <> points
   end
 
   def decode(wkb) do
@@ -73,20 +89,71 @@ defmodule Geo.WKB do
       srid = binary_to_integer(srid, 16)
     end
 
-    if srid != nil do
-      x = Geo.Utils.hex_to_float(String.slice(wkb, 18,16), endian)
-      y = Geo.Utils.hex_to_float(String.slice(wkb, 34,16), endian)
-    else
-      x = Geo.Utils.hex_to_float(String.slice(wkb, 10,16), endian)
-      y = Geo.Utils.hex_to_float(String.slice(wkb, 26,16), endian)
-    end
+    index = if srid != nil, do: 18, else: 10
 
     type = hex_to_type(type &&& 0xff)
+    coordinate_binary = String.slice(wkb, index, String.length(wkb))
 
-    Geometry.new type: type, coordinates: [x, y], srid: srid
+    coordinates = decode_coordinates(type, coordinate_binary, endian)
+    Geometry.new type: type, coordinates: coordinates, srid: srid
   end
 
-  def hex_to_type(type) do
+  defp decode_coordinates(:point, coordinate_binary, endian) do
+    x = String.slice(coordinate_binary, 0,16) |> Geo.Utils.hex_to_float(endian)
+    y = String.slice(coordinate_binary, 16,16) |> Geo.Utils.hex_to_float(endian)
+    [x,y]
+  end
+
+  defp decode_coordinates(:line_string, coordinate_binary, endian) do
+    number_of_points = String.slice(coordinate_binary, 0, 8)
+
+    if endian == :ndr do
+      number_of_points = Geo.Utils.reverse_byte_order(number_of_points)
+    end
+
+    number_of_points = number_of_points |> binary_to_integer(16)
+
+    Enum.map(Enum.to_list(0..(number_of_points-1)), fn(x) ->
+      pair = String.slice(coordinate_binary, 8 + (32 * x), 32)
+      decode_coordinates(:point, pair, endian)
+    end)
+  end
+
+  defp decode_coordinates(:polygon, coordinate_binary, endian) do
+    number_of_lines = String.slice(coordinate_binary, 0, 8)
+
+    if endian == :ndr do
+      number_of_lines = Geo.Utils.reverse_byte_order(number_of_lines)
+    end
+
+    number_of_lines = number_of_lines |> binary_to_integer(16)
+    line_strings = get_line_strings(String.slice(coordinate_binary, 8, String.length(coordinate_binary)), number_of_lines, endian)
+
+    Enum.map(line_strings, fn(x) ->
+      decode_coordinates(:line_string, x, endian)
+    end)
+  end
+
+  def get_line_strings(coordinate_binary, number_of_lines, endian, coordinates \\ []) do
+    if coordinate_binary == "" or number_of_lines <= 0 do
+      coordinates
+    else
+      number_of_points = String.slice(coordinate_binary, 0, 8)
+
+      if endian == :ndr do
+        number_of_points = Geo.Utils.reverse_byte_order(number_of_points)
+      end
+
+      number_of_points = number_of_points |> binary_to_integer(16)
+
+      string = String.slice(coordinate_binary, 0, 8 + (32 * number_of_points))
+      remaining = String.slice(coordinate_binary, 8 + (32 * number_of_points), String.length(coordinate_binary))
+
+      get_line_strings(remaining, number_of_lines - 1, endian, Enum.concat(coordinates, [string]))
+    end
+  end
+
+  defp hex_to_type(type) do
     case type do
       1 ->
         :point
@@ -96,6 +163,21 @@ defmodule Geo.WKB do
         :polygon
       true ->
         :geometry
+    end
+  end
+
+  defp type_to_hex(type, include_srid) do
+    value = if include_srid, do: 0x20000000, else: 0x00000000
+
+    case type do
+      :point ->
+        value + 0x01
+      :line_string ->
+        value + 0x02
+      :polygon ->
+        value + 0x03
+      true ->
+        value + 0x04
     end
   end
 end
