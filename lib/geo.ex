3dd8909562a965a55cc1210c5ac5249dcd8f1aac
Bryan Joseph <bryanjos@gmail.com>
2015-01-27 03:31:25 -0600

    Geometry now implements Ecto.Type

--- lib/geo.ex
+++ lib/geo.ex
@@ -1,12 +1,9 @@
 defmodule Geo do
 
-  defmodule Geometry do
-    defstruct type: :geometry, coordinates: [], srid: nil
-  end
-
   defimpl String.Chars, for: Geo.Geometry do  
     def to_string(geo) do
       Geo.WKT.encode(geo)
     end
   end
+
 end
---
+++ lib/geo/geometry.ex
@@ -0,0 +1,18 @@
+defmodule Geo.Geometry do
+
+  @behaviour Ecto.Type
+  defstruct type: :geometry, coordinates: [], srid: nil
+
+  def type, do: :geometry
+
+  def blank?, do: false
+
+  def load(%Geo.Geometry{} = geo), do: {:ok, geo}
+  def load(_), do: :error
+
+  def dump(%Geo.Geometry{} = geo), do: {:ok, geo}
+  def dump(_), do: :error
+
+  def cast(%Geo.Geometry{} = geo), do: {:ok, geo}
+  def cast(_), do: :error
+end
