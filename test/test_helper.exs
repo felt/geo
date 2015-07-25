3506835c7daec265c9c48f0658c75fba879df77c
JoseLuis Torres <jl@joseluistorres.me>
2015-07-24 20:46:05 -0500

    adding automatic db create and postgis

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,9 @@
 ExUnit.start
+
+opts = [hostname: "localhost",
+   username: "postgres", database: "geo_postgrex_test",
+   extensions: [{Geo.PostGIS.Extension, library: Geo}]]
+
+{:ok, pid} = Postgrex.Connection.start_link(opts)
+{:ok, _} = Postgrex.Connection.query(pid, "CREATE EXTENSION IF NOT EXISTS postgis", [])
+
