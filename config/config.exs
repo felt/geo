066be18c41716d45dc147e77865e6213368344e0
Rafael Rocha <rafaelrochasilva@gmail.com>
2018-02-14 18:05:05 -0200

    Use formater to format the code

--- config/config.exs
+++ config/config.exs
@@ -12,4 +12,4 @@ config :logger, :console,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
--- lib/geo/multi_line_string.ex
+++ lib/geo/multi_line_string.ex
@@ -1,10 +1,9 @@
 defmodule Geo.MultiLineString do
-
   @moduledoc """
   Defines the MultiLineString struct. Implements the Ecto.Type behaviour
   """
 
-  @type t :: %Geo.MultiLineString{ coordinates: [[{number, number}]], srid: integer }
+  @type t :: %Geo.MultiLineString{coordinates: [[{number, number}]], srid: integer}
   defstruct coordinates: [], srid: nil
 
   if Code.ensure_loaded?(Ecto.Type) do
@@ -21,13 +20,15 @@ defmodule Geo.MultiLineString do
     def dump(_), do: :error
 
     def cast(%Geo.MultiLineString{} = multi_line_string), do: {:ok, multi_line_string}
-    def cast(%{"type" => "MultiLineString", "coordinates" => _} = multi_line_string), do: { :ok, Geo.JSON.decode(multi_line_string) }
+
+    def cast(%{"type" => "MultiLineString", "coordinates" => _} = multi_line_string),
+      do: {:ok, Geo.JSON.decode(multi_line_string)}
 
     if Code.ensure_loaded?(Poison) do
-      def cast(multi_line_string) when is_binary(multi_line_string), do: { :ok, Poison.decode!(multi_line_string) |> Geo.JSON.decode }
+      def cast(multi_line_string) when is_binary(multi_line_string),
+        do: {:ok, Poison.decode!(multi_line_string) |> Geo.JSON.decode()}
     end
 
     def cast(_), do: :error
   end
-
 end
