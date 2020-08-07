c6b8fd81ce7dbb0d54ce8139d875d53183429368
Bryan Joseph <bryanjos@gmail.com>
2020-08-07 16:11:17 -0500

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Geo.Mixfile do
   def project do
     [
       app: :geo,
-      version: "3.3.3",
+      version: "3.3.4",
       elixir: "~> 1.6",
       deps: deps(),
       description: description(),
