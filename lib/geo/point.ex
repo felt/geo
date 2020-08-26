fb799495cd7b644a28358b28bfa9d59e1ec1b288
Bryan Joseph <bryanjos@gmail.com>
2020-08-26 18:03:15 -0500

    Update point.ex

--- lib/geo/point.ex
+++ lib/geo/point.ex
@@ -3,8 +3,8 @@ defmodule Geo.Point do
   Defines the Point struct.
   """
 
-  @type latitude :: float
-  @type longitude :: float
+  @type latitude :: number
+  @type longitude :: number
 
   @type t :: %Geo.Point{coordinates: {longitude, latitude}, srid: integer | nil, properties: map}
   defstruct coordinates: {0, 0}, srid: nil, properties: %{}
